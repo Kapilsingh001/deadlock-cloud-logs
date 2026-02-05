845ec32606f6817d2d0989a3cf2551d8d99f307f
Kapilsingh001 <ks1878154@gmail.com>
2026-02-05 21:17:07 +0530

    Initial cloud logging backend

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let logs = [];
+
+// receive log
+app.post("/log", (req, res) => {
+  const time = new Date().toLocaleTimeString();
+  logs.push(`[${time}] ${req.body.message}`);
+  res.json({ status: "ok" });
+});
+
+// send logs
+app.get("/logs", (req, res) => {
+  res.json(logs);
+});
+
+// health check
+app.get("/health", (req, res) => {
+  res.json({ status: "running" });
+});
+
+app.listen(3000, () => {
+  console.log("Cloud server running...");
+});
